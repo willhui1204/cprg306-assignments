0e238d4c9790df254b8aa7c0ea435cbe8840a9f1
willhui1204 <siukee.hui@edu.sait.ca>
2024-05-21 13:29:55 -0600

    May 21 In progress

--- app/week-2/page.js
+++ app/week-2/page.js
@@ -1,9 +1,12 @@
+import Link from "next/link";
+import StudentInfo from "./student-info";
 
 
 export default function Page() {
     return (
       <main>
         <h1>Shopping List</h1>
+        <StudentInfo />
       </main>
     );
   }
---
+++ app/week-2/student-info.js
@@ -0,0 +1,12 @@
+import Link from "next/link";
+
+
+export default function StudentInfo(){
+
+    return(
+        <div>
+            <h2>Will Hui</h2>
+            <p><Link href="https://github.com/willhui1204/cprg306-assignments">https://github.com</Link></p>
+        </div>
+    );
+}
