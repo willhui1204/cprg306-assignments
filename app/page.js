7853418b98adf3c9e022e352b4ddf83a12554f18
willhui1204 <siukee.hui@edu.sait.ca>
2024-05-27 22:02:38 -0600

    Update page.js

--- app/page.js
+++ app/page.js
@@ -2,9 +2,11 @@ import Link from "next/link";
 
 export default function Home(){
   return (
-    <div>
-    <h1>CPRG306: Web Development 2 - Assignments</h1>
-    <p><Link href="./week-2/">Week 2 Assignment</Link></p>
+    <div className="flex min-h-screen bg-black text-white flex-col items-center justify-between pt-24">
+    <div className="z-10 max-w-5xl w-full items-center justify-between text-sm">
+    <h1 className="text-4xl font-bold">CPRG306: Web Development 2 - Assignments</h1>
+    <p className="text-lg"><Link href="./week-2/">Week 2 Assignment</Link></p>
+    </div>
     </div>
   );
 }
