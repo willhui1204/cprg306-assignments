6403a94bb7c0c1e348415667f1b0acbdd2bb158b
willhui1204 <siukee.hui@edu.sait.ca>
2024-06-03 18:20:49 -0600

    week-3 assisgnment

--- app/page.js
+++ app/page.js
@@ -1,12 +1,19 @@
 import Link from "next/link";
 
-export default function Home(){
+export default function Home() {
   return (
     <div className="flex min-h-screen bg-black text-white flex-col items-center justify-between pt-24">
-    <div className="z-10 max-w-5xl w-full items-center justify-between text-sm">
-    <h1 className="text-4xl font-bold">CPRG306: Web Development 2 - Assignments</h1>
-    <p className="text-lg"><Link href="./week-2/">Week 2 Assignment</Link></p>
-    </div>
+      <div className="z-10 max-w-5xl w-full items-center justify-between text-sm">
+        <h1 className="text-4xl font-bold">
+          CPRG306: Web Development 2 - Assignments
+        </h1>
+        <p className="text-lg">
+          <Link href="./week-2/">Week 2 Assignment</Link>
+        </p>
+        <p className="text-lg">
+          <Link href="./week-3/">Week 3 Assignment</Link>
+        </p>
+      </div>
     </div>
   );
 }
---
+++ app/week-3/item.js
@@ -0,0 +1,12 @@
+
+
+export default function Item({ name, quantity, category }) {
+  return (
+    <div className="p-2 m-4 bg-slate-900 text-white max-w-sm">
+      <h2 className="font-bold text-xl">{name}</h2>
+      <p className="text-white text-sm">
+        Buy {quantity} in {category}
+      </p>
+    </div>
+  );
+}
